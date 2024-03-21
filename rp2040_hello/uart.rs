21a25e17e3cd7738516bc211e86c9e542c07d8e7
Mateusz Stadnik <matgla@live.com>
2024-03-21 21:38:27 +0100

    initial commit

---
+++ rp2040_hello/uart.rs
@@ -0,0 +1,75 @@
+#![no_std]
+#![no_main]
+
+use panic_halt as _;
+
+use core::fmt::Write;
+use hal::pac;
+use rp2040_hal as hal;
+
+// use core::fmt::Write;
+use fugit::RateExtU32;
+use rp2040_hal::clocks::Clock;
+
+use hal::uart::{DataBits, StopBits, UartConfig};
+
+#[link_section = ".boot2"]
+#[used]
+pub static BOOT2: [u8; 256] = rp2040_boot2::BOOT_LOADER_GENERIC_03H;
+
+const XTAL_FREQ_HZ: u32 = 12_000_000u32;
+
+#[rp2040_hal::entry]
+fn main() -> ! {
+    let mut pac = pac::Peripherals::take().unwrap();
+    let core = pac::CorePeripherals::take().unwrap();
+
+    let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
+
+    let clocks = hal::clocks::init_clocks_and_plls(
+        XTAL_FREQ_HZ,
+        pac.XOSC,
+        pac.CLOCKS,
+        pac.PLL_SYS,
+        pac.PLL_USB,
+        &mut pac.RESETS,
+        &mut watchdog,
+    )
+    .ok()
+    .unwrap();
+
+    let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
+    let sio = hal::Sio::new(pac.SIO);
+
+    let pins = hal::gpio::Pins::new(
+        pac.IO_BANK0,
+        pac.PADS_BANK0,
+        sio.gpio_bank0,
+        &mut pac.RESETS,
+    );
+
+    let uart_pins = (
+        pins.gpio0.into_function::<hal::gpio::FunctionUart>(),
+        pins.gpio1.into_function::<hal::gpio::FunctionUart>(),
+    );
+
+    let mut uart = hal::uart::UartPeripheral::new(pac.UART0, uart_pins, &mut pac.RESETS)
+        .enable(
+            UartConfig::new(9600.Hz(), DataBits::Eight, None, StopBits::One),
+            clocks.peripheral_clock.freq(),
+        )
+        .unwrap();
+
+    uart.write_full_blocking(b"Uart Example\n");
+    let mut value = 0u32;
+    loop {
+        writeln!(
+            uart,
+            "value: {value:02}, freq: {}\n",
+            clocks.system_clock.freq().to_Hz()
+        )
+        .unwrap();
+        delay.delay_ms(100);
+        value += 1;
+    }
+}
